f39b6a1a2af55293aa6a6688374160ec7f974071
Yuriy Bakhtin <yurybakh@gmail.com>
2024-04-23 17:00:52 +0200

    Unit test

---
+++ tests/codeception/unit/TextEditorTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+namespace text_editor;
+
+use humhub\modules\file\models\File;
+use humhub\modules\text_editor\models\CreateFile;
+use humhub\modules\text_editor\models\forms\ConfigForm;
+use humhub\modules\text_editor\Module;
+use Yii;
+
+class TextEditorTest extends UnitTester
+{
+    public function testTextEditor()
+    {
+        /* @var Module $module */
+        $module = Yii::$app->getModule('text-editor');
+
+        $this->assertFalse($module->canCreate());
+
+        $config = new ConfigForm();
+        $config->allowNewFiles = true;
+        $config->save();
+
+        $this->assertTrue($module->canCreate());
+
+        $createFile = new CreateFile();
+        $createFile->fileName = 'test.any';
+        $testFile = $createFile->save();
+
+        $this->assertTrue($module->canView($testFile));
+        $this->assertTrue($module->canEdit($testFile));
+
+        $testFile->mime_type = 'image/jpeg';
+
+        $this->assertFalse($module->canView($testFile));
+        $this->assertFalse($module->canEdit($testFile));
+    }
+}
