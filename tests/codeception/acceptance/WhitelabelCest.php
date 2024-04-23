0939d7148e8b6a79f0298a6968490c5ad9db6e87
Yuriy Bakhtin <yurybakh@gmail.com>
2024-04-23 16:43:01 +0200

    Add tests

---
+++ tests/codeception/acceptance/WhitelabelCest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+namespace text_editor\acceptance;
+
+use text_editor\AcceptanceTester;
+
+class TextEditorCest
+{
+    public function testTextEditor(AcceptanceTester $I)
+    {
+    }
+}
---
+++ tests/codeception/unit/WhitelabelTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+namespace text_editor;
+
+class TextEditorTest extends UnitTester
+{
+    public function testTextEditor()
+    {
+    }
+}
